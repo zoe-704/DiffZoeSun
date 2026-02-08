08bec0bc9c912b4c43a2960dcdc089378feb9fac
zoe-704 <sunzoey@gmail.com>
2026-02-08 14:37:00 -0800

    v1

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -20,10 +20,15 @@ public class PlagiarismChecker {
         int n = doc1.length(), m = doc2.length(), maxLen = 0;
         int[][] dp = new int[n+2][m+2];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (doc1.charAt(i) == doc2.charAt(j)) {
+        for (int i = 1; i <= n; i++) { // doc1
+            for (int j = 1; j <= m; j++) { // doc2
+                // matches --> +1
+                if (doc1.charAt(i-1) == doc2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                // does not match --> max of 2 prev
+                else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
                 maxLen = Math.max(maxLen, dp[i][j]);
             }
