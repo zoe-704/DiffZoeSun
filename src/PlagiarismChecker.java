b6f9c2983c77f4a2ce384d4cb86eafbb10bd0001
zoe-704 <sunzoey@gmail.com>
2026-02-10 14:06:02 -0800

    final version

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * Plagiarism Checker
  * A tool for finding the longest shared substring between two documents.
@@ -17,19 +15,22 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
+        // variables to find longest common sequence
         int n = doc1.length(), m = doc2.length(), maxLen = 0;
         int[][] dp = new int[n+2][m+2];
-
-        for (int i = 1; i <= n; i++) { // doc1
-            for (int j = 1; j <= m; j++) { // doc2
-                // matches --> +1
+        // iterate through doc 1
+        for (int i = 1; i <= n; i++) {
+            // iterate through doc 2
+            for (int j = 1; j <= m; j++) {
+                // if characters at indexes match, increment by 1
                 if (doc1.charAt(i-1) == doc2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }
-                // does not match --> max of 2 prev
+                // if characters do not match, take max of previous 2 subsequences
                 else {
                     dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
+                // update max length of longest common subsequences
                 maxLen = Math.max(maxLen, dp[i][j]);
             }
         }
